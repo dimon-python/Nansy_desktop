08e6c37328276d6a38bfb5f7fb250092cbcb9ecc
Xvs <xvs.site@gmail.com>
2025-12-21 17:04:11 +0300

    first commit

---
+++ src/main/java/com/example/Nansy_desktop/NansyDesktopApplication.java
@@ -0,0 +1,13 @@
+package com.example.Nansy_desktop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NansyDesktopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NansyDesktopApplication.class, args);
+	}
+
+}
